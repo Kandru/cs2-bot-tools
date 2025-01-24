fd87bab88c432a3b041f720c7da6bc427bc480c6
Kalle <info@kalleminkner.de>
2025-01-24 14:05:41 +0100

    bump version to 0.0.3

--- src/Version.cs
+++ src/Version.cs
@@ -4,6 +4,6 @@ namespace BotTools
 {
     public partial class BotTools : BasePlugin
     {
-        public override string ModuleVersion => "0.0.2";
+        public override string ModuleVersion => "0.0.3";
     }
 }
