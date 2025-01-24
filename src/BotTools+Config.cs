2f67ab7fba1ab12f812e8fc55230fe97801cbf6d
Kalle <info@kalleminkner.de>
2025-01-24 13:43:11 +0100

    fix: remove map-specific configuration

--- src/BotTools+Config.cs
+++ src/BotTools+Config.cs
@@ -6,15 +6,6 @@ using CounterStrikeSharp.API.Modules.Config;
 
 namespace BotTools
 {
-    public class MapConfig
-    {
-        // disabled
-        [JsonPropertyName("enabled")] public bool Enabled { get; set; } = true;
-        // bot names
-        [JsonPropertyName("bot_names")] public List<string> BotNames { get; set; } = [];
-        [JsonPropertyName("bot_clantag")] public string BotClantag { get; set; } = "";
-    }
-
     public class PluginConfig : BasePluginConfig
     {
         // disabled
@@ -36,50 +27,17 @@ namespace BotTools
             "Theo Loge", "Tom Bohler", "Tom Mate", "Volker Racho", "Wanda Lismus", "Wilma Bier", "Wilma Gern"
         ];
         [JsonPropertyName("bot_clantag")] public string BotClantag { get; set; } = "";
-        // map configurations
-        [JsonPropertyName("maps")] public Dictionary<string, MapConfig> MapConfigs { get; set; } = new Dictionary<string, MapConfig>();
     }
 
     public partial class BotTools : BasePlugin, IPluginConfig<PluginConfig>
     {
         public PluginConfig Config { get; set; } = null!;
-        private MapConfig[] _currentMapConfigs = Array.Empty<MapConfig>();
         private string _configPath = "";
 
         private void LoadConfig()
         {
             Config = ConfigManager.Load<PluginConfig>("BotTools");
             _configPath = Path.Combine(ModuleDirectory, $"../../configs/plugins/BotTools/BotTools.json");
-        }
-
-        private void InitializeConfig(string mapName)
-        {
-            // select map configs whose regexes (keys) match against the map name
-            _currentMapConfigs = (from mapConfig in Config.MapConfigs
-                                  where FileSystemName.MatchesSimpleExpression(mapConfig.Key, mapName)
-                                  select mapConfig.Value).ToArray();
-
-            if (_currentMapConfigs.Length > 0)
-            {
-                if (Config.MapConfigs.TryGetValue("default", out var config))
-                {
-                    // add default configuration
-                    _currentMapConfigs = new[] { config };
-                    Console.WriteLine(Localizer["core.defaultconfig"].Value.Replace("{mapName}", mapName));
-                }
-                else
-                {
-                    // there is no config to apply
-                    Console.WriteLine(Localizer["core.noconfig"].Value.Replace("{mapName}", mapName));
-                }
-            }
-            else
-            {
-                Console.WriteLine(Localizer["core.defaultconfig"].Value.Replace("{mapName}", mapName));
-                // create default configuration
-                Config.MapConfigs.Add(mapName, new MapConfig());
-            }
-            Console.WriteLine(Localizer["core.foundconfig"].Value.Replace("{count}", _currentMapConfigs.Length.ToString()).Replace("{mapName}", mapName));
         }
 
         public void OnConfigParsed(PluginConfig config)
